438e4becb8d786083e811bb7c346e511807532e7
Ivan Garcia <97042355+IGarcia2000@users.noreply.github.com>
2022-07-01 18:54:14 -0500

    Update Lab3619.py

--- Basico/Modulo3/Lab3619.py
+++ Basico/Modulo3/Lab3619.py
@@ -1,5 +1,5 @@
 #Autor: Christopher Ivan Garcia Avila
-#Fecha: 15 de Junio de 2022
+#Fecha: 1 de Julio de 2022
 #Descripcion: Manejo de listas sin repeticiones
 
 my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
