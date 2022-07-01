53a1e4eed0185fe4f6377560ec1e188707263aab
Ivan Garcia <97042355+IGarcia2000@users.noreply.github.com>
2022-07-01 18:37:46 -0500

    Update Lab31112.py

--- Basico/Modulo3/Lab31112.py
+++ Basico/Modulo3/Lab31112.py
@@ -1,6 +1,6 @@
-# Nombre: Pablo Uriel Rosas Vargas
-# Fecha: 15 de Junio de 2022
-# Descripcion: Uso de if-elif-else para años bisiestos
+#Autor: Christopher Ivan Garcia Avila
+#Fecha: 15 de Junio de 2022
+#Descripcion: Uso de if-elif-else para años bisiestos
 
 year = int(input("Introduce un año: "))
 
