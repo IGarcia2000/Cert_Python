7075d3c8eadd886d6e6ca5d5c271f3cab6f9f2e8
Ivan Garcia <97042355+IGarcia2000@users.noreply.github.com>
2022-07-01 18:53:35 -0500

    Update Lab3219.py

--- Basico/Modulo3/Lab3219.py
+++ Basico/Modulo3/Lab3219.py
@@ -1,5 +1,5 @@
 #Autor: Christopher Ivan Garcia Avila
-#Fecha: 15 de Junio de 2022
+#Fecha: 1 de Julio de 2022
 #Descripcion: Usar el while con el break 
 
 word = input("Ingresa una palabra o ingresa la palabra secreta: ")
