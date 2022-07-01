7976fcf3af2e05e58d25c379660a7fb3537275ee
Ivan Garcia <97042355+IGarcia2000@users.noreply.github.com>
2022-07-01 18:14:46 -0500

    Delete Modulo2 directory

--- Modulo2/Lab2116.py
+++
@@ -1,6 +0,0 @@
-#Autor: Pablo Uriel Rosas Vargas
-#Fecha: 14 de Junio de 2022
-#Descripcion: Hola mundo
-
-print("Hola Mundo!") #Aqui imprimimos un hola mundo
-print("Pablo Uriel Rosas Vargas") #Aqui imprimimos nuestro nombre
