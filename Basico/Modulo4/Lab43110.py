98ea3346faac1b54344c3bb378882e3790047e27
Ivan Garcia <97042355+IGarcia2000@users.noreply.github.com>
2022-07-01 18:46:48 -0500

    Update Lab43110.py

--- Basico/Modulo4/Lab43110.py
+++ Basico/Modulo4/Lab43110.py
@@ -1,6 +1,6 @@
-# Nombre: Pablo Uriel Rosas Vargas
-# Fecha: 15 de Junio de 2022
-# Descripcion: Saber si un numero es primo o impar
+#Autor: Christopher Ivan Garcia Avila
+#Fecha: 1 de Julio de 2022
+#Descripcion: Saber si un numero es primo o impar
 
 # 1 milla (mile) = 1609.344 metros(metres)
 # 1 galón (gallon) = 3.785411784 litros(litres)
