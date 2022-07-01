b592d4f229b7d01b571a5000951b712b6b13ec3e
Ivan Garcia <97042355+IGarcia2000@users.noreply.github.com>
2022-07-01 18:41:31 -0500

    Update Lab3216.py

--- Basico/Modulo3/Lab3216.py
+++ Basico/Modulo3/Lab3216.py
@@ -1,6 +1,6 @@
-# Nombre: Pablo Uriel Rosas Vargas
-# Fecha: 15 de Junio de 2022
-# Descripcion: Usar de forma basica el ciclo for
+#Autor: Christopher Ivan Garcia Avila
+#Fecha: 15 de Junio de 2022
+#Descripcion: Usar de forma basica el ciclo for
 import time
 
 for i in range(1, 6): # Escribe un bucle for que cuente hasta cinco.
