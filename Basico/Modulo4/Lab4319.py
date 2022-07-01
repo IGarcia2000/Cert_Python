59f686aa40f435fcd4f35cb321fc3bc99bf3e29f
Ivan Garcia <97042355+IGarcia2000@users.noreply.github.com>
2022-07-01 18:49:19 -0500

    Update Lab4319.py

--- Basico/Modulo4/Lab4319.py
+++ Basico/Modulo4/Lab4319.py
@@ -1,6 +1,6 @@
-# Nombre: Pablo Uriel Rosas Vargas
-# Fecha: 15 de Junio de 2022
-# Descripcion: Saber si un numero es primo o impar
+#Autor: Christopher Ivan Garcia Avila
+#Fecha: 1 de Julio de 2022
+#Descripcion: Saber si un numero es primo o impar
 
 def is_prime(num):
     for i in range(2, int(1 + num ** 0.5)):
